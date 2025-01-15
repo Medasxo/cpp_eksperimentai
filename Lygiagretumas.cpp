fb8bd21989769fb4132b3210695dada0b9bc3bb7
Medasxo <70206510+Medasxo@users.noreply.github.com>
2025-01-15 22:09:22 +0200

    atnaujinti lygiagretumo laboratorini

--- Lygiagretumas.cpp
+++ Lygiagretumas.cpp
@@ -4,12 +4,13 @@
 #include <numeric>
 #include "Laikmatis.h"
 
-static void sumuoti(const std::vector<int>& duomenys, size_t start, size_t end, int& rezultatas) {
-    rezultatas = std::accumulate(duomenys.begin() + start, duomenys.begin() + end, 0);
+static void skaiciuotiSuma(const std::vector<int>& duomenys, int pradzia, int pabaiga, int& rezultatas)
+{
+    rezultatas = std::accumulate(duomenys.begin() + pradzia, duomenys.begin() + pabaiga, 0);
 }
 
 void tirtiLygiagretuma() {
-    const size_t duomenuDydis = 1e9;
+    const size_t duomenuDydis = 1e8;
     const int reiksme = 1;
     std::vector<int> duomenys(duomenuDydis, reiksme);
 
@@ -23,8 +24,8 @@ void tirtiLygiagretuma() {
         std::cout << "Dvigijis skaiciavimas:\n";
         int rezultatas1 = 0, rezultatas2 = 0;
         Laikmatis laikmatis;
-        std::thread gija1(sumuoti, std::cref(duomenys), 0, duomenys.size() / 2, std::ref(rezultatas1));
-        std::thread gija2(sumuoti, std::cref(duomenys), duomenys.size() / 2, duomenys.size(), std::ref(rezultatas2));
+        std::thread gija1(skaiciuotiSuma, std::cref(duomenys), 0, duomenys.size() / 2, std::ref(rezultatas1));
+        std::thread gija2(skaiciuotiSuma, std::cref(duomenys), duomenys.size() / 2, duomenys.size(), std::ref(rezultatas2));
         gija1.join();
         gija2.join();
         int rezultatas = rezultatas1 + rezultatas2;
@@ -34,10 +35,10 @@ void tirtiLygiagretuma() {
         std::cout << "Keturgijis skaiciavimas:\n";
         int rezultatas1 = 0, rezultatas2 = 0, rezultatas3 = 0, rezultatas4 = 0;
         Laikmatis laikmatis;
-        std::thread gija1(sumuoti, std::cref(duomenys), 0, duomenys.size() / 4, std::ref(rezultatas1));
-        std::thread gija2(sumuoti, std::cref(duomenys), duomenys.size() / 4, duomenys.size() / 2, std::ref(rezultatas2));
-        std::thread gija3(sumuoti, std::cref(duomenys), duomenys.size() / 2, 3 * duomenys.size() / 4, std::ref(rezultatas3));
-        std::thread gija4(sumuoti, std::cref(duomenys), 3 * duomenys.size() / 4, duomenys.size(), std::ref(rezultatas4));
+        std::thread gija1(skaiciuotiSuma, std::cref(duomenys), 0, duomenys.size() / 4, std::ref(rezultatas1));
+        std::thread gija2(skaiciuotiSuma, std::cref(duomenys), duomenys.size() / 4, duomenys.size() / 2, std::ref(rezultatas2));
+        std::thread gija3(skaiciuotiSuma, std::cref(duomenys), duomenys.size() / 2, 3 * duomenys.size() / 4, std::ref(rezultatas3));
+        std::thread gija4(skaiciuotiSuma, std::cref(duomenys), 3 * duomenys.size() / 4, duomenys.size(), std::ref(rezultatas4));
         gija1.join();
         gija2.join();
         gija3.join();
